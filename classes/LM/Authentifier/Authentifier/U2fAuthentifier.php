aa1ba3877af0e55f21e7fc157d230a1980296a6c
Louis-Marie Matthews <matthewslouismarie@gmail.com>
2018-04-03 19:24:03 +0100

    Add asset function

--- classes/LM/Authentifier/Authentifier/U2fAuthentifier.php
+++ classes/LM/Authentifier/Authentifier/U2fAuthentifier.php
@@ -6,17 +6,27 @@ use GuzzleHttp\Psr7\Response;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\RequestInterface;
 use Twig_Environment;
+use Twig_Function;
 use Twig_Loader_Filesystem;
+use LM\Authentifier\Configuration\IConfiguration;
 
 class U2fAuthentifier implements IAuthentifier
 {
+    private $config;
+
+    public function __construct(IConfiguration $config)
+    {
+        $this->config = $config;
+    }
+
     public function process(RequestInterface $request): ResponseInterface
     {
         $loader = new Twig_Loader_Filesystem(__DIR__."/../../../../templates");
-        $twig = new Twig_Environment($loader, array(
-            "cache" => __DIR__."../../../../.matthewslouismarie/authentifier/twig_cache",
-        ));
-
+        $twig = new Twig_Environment($loader, [
+            "cache" => false,
+        ]);
+        $assetFunction = new Twig_Function("asset", [$this->config, "getAssetUri"]);
+        $twig->addFunction($assetFunction);
         return new Response(200, [], $twig->render("u2f.html.twig"));
     }
 }
