d623857d52a3346be70b27994e2029e452c85b06
Louis-Marie Matthews <matthewslouismarie@gmail.com>
2018-04-03 18:22:59 +0100

    Render dummy template

--- classes/LM/Authentifier/Authentifier/U2fAuthentifier.php
+++ classes/LM/Authentifier/Authentifier/U2fAuthentifier.php
@@ -5,15 +5,18 @@ namespace LM\Authentifier\Authentifier;
 use GuzzleHttp\Psr7\Response;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\RequestInterface;
+use Twig_Environment;
+use Twig_Loader_Filesystem;
 
 class U2fAuthentifier implements IAuthentifier
 {
     public function process(RequestInterface $request): ResponseInterface
     {
-        $loader = new Twig_Loader_Filesystem("/templates");
+        $loader = new Twig_Loader_Filesystem(__DIR__."/../../../../templates");
         $twig = new Twig_Environment($loader, array(
-            "cache" => "/cache",
+            "cache" => __DIR__."../../../../.matthewslouismarie/authentifier/twig_cache",
         ));
+
         return new Response(200, [], $twig->render("u2f.html.twig"));
     }
 }
