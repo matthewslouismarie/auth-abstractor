424381bd9723b499b63390ebd17f162ea79f1ee9
Louis-Marie Matthews <matthewslouismarie@gmail.com>
2018-04-06 18:23:24 +0100

    Add unused ChallengeDefinition class (might be used later)

---
+++ src/LM/Authentifier/Model/ChallengeDefinition.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace LM\Authentifier\Model;
+
+use InvalidArgumentException;
+use LM\Authentifier\Challenge\IChallenge;
+use Serializable;
+
+/**
+ * @todo Will later be used to specify a maximum number of attempts per
+ * challenge.
+ * Not used yet. Might be in the future.
+ */
+class ChallengeDefinition implements Serializable
+{
+    private $classname;
+
+    private $number;
+
+    public function __construct(string $classname, int $number = 1)
+    {
+        if (!in_array(IChallenge::class, class_implements($classname), true)) {
+            throw new InvalidArgumentException();
+        } elseif ($number < 1) {
+            throw new InvalidArgumentException();
+        }
+        $this->classname = $classname;
+        $this->number = $number;
+    }
+
+    public function getClassname(): string
+    {
+        return $this->classname;
+    }
+
+    public function getNumber(): int
+    {
+        return $this->number;
+    }
+
+    public function serialize()
+    {
+        return serialize([
+            $this->classname,
+            $this->number,
+        ]);
+    }
+
+    public function unserialize($serialized)
+    {
+        list(
+            $this->classname,
+            $this->number) = unserialize($serialized);
+    }
+}
