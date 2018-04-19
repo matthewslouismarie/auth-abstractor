426c2cdc25dfe4b0a8fd5749262fdae43051bfdf
Louis-Marie Matthews <matthewslouismarie@gmail.com>
2018-04-19 13:15:50 +0100

    Add convenience implementation of IApplicationConfiguration

---
+++ tests/ApplicationConfigurationTest.php
@@ -0,0 +1,83 @@
+<?php
+
+use LM\Authentifier\Implementation\ApplicationConfiguration;
+use LM\Authentifier\Implementation\Member;
+use LM\Authentifier\Implementation\TestingTokenStorage;
+use PHPUnit\Framework\TestCase;
+
+class ApplicationConfigurationTest extends TestCase
+{
+    /**
+     * @todo Test with invalid array.
+     */
+    public function testApplicationConfiguration()
+    {
+        $pwdConfig = [
+            'min_length' => 5,
+            'enforce_min_length' => true,
+            'uppercase' => false,
+            'special_chars' => false,
+            'numbers' => false,
+        ];
+        $configuration = new ApplicationConfiguration(
+            'https://example.org',
+            'https://example.org',
+            '/var/www/html/example/vendor',
+            null,
+            null,
+            function (string $username): bool {
+                return 'user0' === $username ? true : false;
+            },
+            function (string $username): Member {
+                return new Member(password_hash('pwd', PASSWORD_DEFAULT), $username);
+            },
+            $pwdConfig,
+            new TestingTokenStorage(realpath(__DIR__.'/..')),
+            function (string $username): array {
+                return [];
+            }
+        );
+        $this->assertSame(
+            'https://example.org',
+            $configuration->getAppId()
+        );
+        $this->assertSame(
+            'https://example.org/jquery.min.js',
+            $configuration->getAssetUri('jquery.min.js')
+        );
+        $this->assertSame(
+            '/var/www/html/example/vendor',
+            $configuration->getComposerDir()
+        );
+        $this->assertNull($configuration->getContainer());
+        $this->assertNull($configuration->getCustomTwigDir());
+        $this->assertSame(
+            'user0',
+            $configuration->getMember('user0')->getUsername()
+        );
+        $this->assertSame(
+            $pwdConfig,
+            $configuration->getPwdSettings()
+        );
+        $configuration
+            ->getTokenStorage()
+            ->setToken('csrf', 'blabla')
+        ;
+        $this->assertSame(
+            'blabla',
+            $configuration->getTokenStorage()->getToken('csrf')
+        );
+        $this->assertSame(
+            [],
+            $configuration->getU2fRegistrations('user0')
+        );
+        $this->assertSame(
+            $configuration->isExistingMember('user0'),
+            true
+        );
+        $this->assertSame(
+            $configuration->isExistingMember('user1'),
+            false
+        );
+    }
+}
