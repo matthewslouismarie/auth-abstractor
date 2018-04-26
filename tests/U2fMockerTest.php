485267aec9d586db0a05fce4b8546ce2d43df280
Louis-Marie Matthews <matthewslouismarie@gmail.com>
2018-04-26 22:15:31 +0000

    Test U2fMockerTest more extensively

--- tests/U2fMockerTest.php
+++ tests/U2fMockerTest.php
@@ -4,16 +4,21 @@ declare(strict_types=1);
 
 namespace Tests\LM;
 
+use Firehed\U2f\RegisterRequest;
 use InvalidArgumentException;
 use LM\AuthAbstractor\Implementation\Member;
 use LM\Common\DataStructure\TypedMap;
 use LM\Common\Enum\Scalar;
 use LM\AuthAbstractor\Test\KernelMocker;
 use PHPUnit\Framework\TestCase;
+use LM\AuthAbstractor\Model\IU2fRegistration;
 use LM\AuthAbstractor\Mocker\U2fMocker;
 
 class U2fMockerTest extends TestCase
 {
+    /**
+     * @todo Use PHPUnit type tests.
+     */
     public function testU2fMocker()
     {
         $kernel = (new KernelMocker())->getKernel();
@@ -27,5 +32,29 @@ class U2fMockerTest extends TestCase
         );
 
         $this->assertSame($nU2fRegistrations, count($u2fMocker->getU2fRegistrations()));
+
+        $u2fRegData = $u2fMocker->getU2fRegistrations();
+        foreach ($u2fRegData as $id => $u2fRegDatum) {
+            $this->assertTrue(is_int($id));
+            $this->assertTrue(isset($u2fRegDatum['u2fRegistration']));
+            $this->assertTrue(is_a($u2fRegDatum['u2fRegistration'], IU2fRegistration::class));
+            if (isset($u2fRegDatum['registerResponseStr'])) {
+                $this->assertTrue($this->isValidJson($u2fRegDatum['registerResponseStr']));
+            }
+            if (isset($u2fRegDatum['registerRequest'])) {
+                $this->assertTrue(is_a($u2fRegDatum['registerRequest'], RegisterRequest::class));
+            }
+        }
+    }
+
+    /**
+     * @link https://stackoverflow.com/a/6041773/7089212
+     * @todo Move in a service?
+     */
+    public function isValidJson($string): bool
+    {
+        json_decode($string);
+
+        return (json_last_error() === JSON_ERROR_NONE);
     }
 }
