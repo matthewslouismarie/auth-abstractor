3d5f44ea86debcc8ce9b7465cb2a28092cc60a4a
Louis-Marie Matthews <matthewslouismarie@gmail.com>
2018-04-26 23:13:17 +0000

    Attempt to activate codecov

--- check-code-quality.sh
+++ check-code-quality.sh
@@ -1,3 +1,3 @@
 vendor/bin/php-cs-fixer fix --dry-run --allow-risky=yes
-vendor/bin/phpunit
-vendor/bin/infection --min-covered-msi=70 --threads=4
+vendor/bin/phpunit --coverage-clover=clover.xml --coverage-xml=coverage/coverage-xml --log-junit=coverage/phpunit.junit.xml
+vendor/bin/infection --coverage=coverage --min-covered-msi=70 --threads=4
