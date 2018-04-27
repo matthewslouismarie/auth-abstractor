e406a1fc2690158fd617441e4da134c9b25a5dbf
Louis-Marie Matthews <matthewslouismarie@gmail.com>
2018-04-27 00:34:21 +0000

    CredentialRegistrationChallenge checks for the password complexity

---
+++ src/LM/AuthAbstractor/Implementation/EmptyCallback.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LM\AuthAbstractor\Implementation;
+
+use Closure;
+use LM\AuthAbstractor\Model\AuthenticationProcess;
+use Psr\Http\Message\ResponseInterface;
+use LM\AuthAbstractor\Model\IAuthenticationCallback;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bridge\PsrHttpMessage\Factory\DiactorosFactory;
+
+/**
+ * This is a convenience implementation of IAuthenticationCallback for testing.
+ * It returns an empty HTTP response.
+ */
+class EmptyCallback implements IAuthenticationCallback
+{
+    private $diactoros;
+
+    public function __construct()
+    {
+        $this->diactoros = new DiactorosFactory();
+    }
+
+    public function handleFailedProcess(AuthenticationProcess $authProcess): ResponseInterface
+    {
+        return $this->diactoros->createResponse(new Response());
+    }
+
+    public function handleSuccessfulProcess(AuthenticationProcess $authProcess): ResponseInterface
+    {
+        return $this->diactoros->createResponse(new Response());
+    }
+}
