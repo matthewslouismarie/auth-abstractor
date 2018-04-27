ed13635ebda727326c43715bd7d6e5e8eed6c799
Louis-Marie Matthews <matthewslouismarie@gmail.com>
2018-04-27 20:19:38 +0100

    Restore unit test for AuthenticationProcess

--- tests/AuthenticationProcessTest.php
+++ tests/AuthenticationProcessTest.php
@@ -5,20 +5,17 @@ declare(strict_types=1);
 namespace Tests\LM;
 
 use LM\AuthAbstractor\Enum\AuthenticationProcess\Status;
-use LM\AuthAbstractor\Mocker\U2fMocker;
 use LM\AuthAbstractor\Model\AuthenticationProcess;
-use LM\AuthAbstractor\Factory\AuthenticationProcessFactory;
 use LM\Common\DataStructure\TypedMap;
 use LM\Common\Enum\Scalar;
 use LM\Common\Model\ArrayObject;
+use PHPUnit\Framework\TestCase;
 use LM\Common\Model\StringObject;
 
-class AuthenticationProcessTest extends LibTestCase
+class AuthenticationProcessTest extends TestCase
 {
     public function testSerialization()
     {
-        $this->markTestSkipped('Must be updated.');
-
         $authentifiers = [
             ExistingUsernameChallenge::class,
             U2fChallenge::class,
@@ -40,43 +37,5 @@ class AuthenticationProcessTest extends LibTestCase
             U2fChallenge::class,
             $unserializedProcess->getCurrentChallenge()
         );
-    }
-
-    public function testU2fRegistrations()
-    {
-        $this->markTestSkipped('Must be updated.');
-
-        $this->assertNotNull($this->createKernel());
-        $mocker = $this->get(U2fMocker::class);
-        $process = $this
-            ->get(AuthenticationProcessFactory::class)
-            ->createProcess(
-                [
-                    U2fRegistrationChallenge::class,
-                ],
-                3
-            )
-        ;
-        for ($i = 0; $i < 2; $i++) {
-            $u2fRegistrations = $process
-                ->getTypedMap()
-                ->get('u2f_registrations', Scalar::_ARRAY)
-            ;
-            $u2fRegistrations[] = $mocker->get($i);
-            $serialized = serialize(new AuthenticationProcess(
-                $process
-                ->getTypedMap()
-                ->set(
-                    'u2f_registrations',
-                    $u2fRegistrations,
-                    Scalar::_ARRAY
-                )
-            ));
-            $process = unserialize($serialized);
-            $process
-                ->getTypedMap()
-                ->get('u2f_registrations', Scalar::_ARRAY)
-            ;
-        }
     }
 }
